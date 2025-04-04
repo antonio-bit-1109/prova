4b365ab66668a84302a61cd4eb40e77649740b73
francesco <francescocarlini36@gmail.com>
2025-04-04 14:28:06 +0200

    Eliminato costruttore

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,14 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  constructor() {
-    console.log('Ciao');
-    console.log('Ciao');
-    console.log('Ciao');
-  }
-
-  stampaMessaggio() {
-    console.log('Prova commit');
-  }
-}
+export class HomeComponent {}
